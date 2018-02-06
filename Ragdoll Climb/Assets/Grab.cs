d95ad0ea1bb20b0fccc7e01a725609d4b5a226b0
Dennis Karlsson <dennis.k9705@gmail.com>
2018-02-06 18:03:07 +0100

    - Movment for arms now has basic funcyionality. - Added folders. - Added player prefab.

---
+++ Ragdoll Climb/Assets/Grab.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grab : MonoBehaviour
+{
+    bool canGripLeft = false;
+    bool released = true;
+    
+    FixedJoint joint;
+
+
+	void Start ()
+    {
+		
+	}
+	
+
+	void Update ()
+    {
+        if (Input.GetAxis("XB-leftTrigger") == 1 && canGripLeft && released)
+        {
+            joint.connectedBody = GetComponent<Rigidbody>();
+
+            released = false;
+        }
+
+        if (Input.GetAxis("XB-leftTrigger") == 0 && !released)
+        {
+            joint.connectedBody = null;
+
+            released = true;
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "ClimbObj")
+        {
+            joint = other.GetComponent<FixedJoint>();
+
+            canGripLeft = true;
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "ClimbObj")
+        {
+            joint = null;
+
+            canGripLeft = false;
+        }
+    }
+}
---
+++ Ragdoll Climb/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float pushForce = 100f;
+
+    [SerializeField] Rigidbody leftHand;
+    [SerializeField] Rigidbody rightHand;
+
+    bool gripLeft = false;
+    bool gripRight = false;
+    bool canGripLeft = false;
+    bool canGripRight = false;
+
+    Vector3 pushDirLeft;
+    Vector3 pushDirRight;
+
+
+	void Start ()
+    {
+		
+	}
+	
+
+	void Update ()
+    {
+        if (!gripLeft)
+            pushDirLeft = new Vector3(Input.GetAxis("XB-leftjoystickX"), -Input.GetAxis("XB-leftjoystickY"), 0f);
+        else
+            pushDirLeft = Vector3.zero;
+        if (!gripRight)
+            pushDirRight = new Vector3(Input.GetAxis("XB-rightjoystickX"), -Input.GetAxis("XB-rightjoystickY"), 0f);
+        else
+            pushDirRight = Vector3.zero;
+        
+        
+    }
+
+
+    private void FixedUpdate()
+    {
+        leftHand.AddForce(pushDirLeft * pushForce);
+        rightHand.AddForce(pushDirRight * pushForce);
+    }
+}
