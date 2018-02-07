9f48ea8e73034ad9c49ad368644fdedab291332a
UTB\Ohlsab <eyber4@gmail.com>
2018-02-07 14:30:30 +0100

    Added a Level and water and a boat. Camera that look between two players and follow them.

---
+++ Ragdoll Climb/Assets/Test/CameraScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject[] players;
+
+    public float test;
+
+    public float cameraPos;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (players[0].transform.position.y < players[1].transform.position.y) {
+            test =  players[0].transform.position.y;
+
+            cameraPos = (((players[1].transform.position.y - players[0].transform.position.y)/2) + test);
+
+            transform.position = new Vector3(transform.position.x, cameraPos, transform.position.z);
+
+            Debug.Log("i första ");
+        }
+
+        else if (players[1].transform.position.y < players[0].transform.position.y) {
+            test = players[1].transform.position.y;
+
+            cameraPos = (((players[0].transform.position.y - players[1].transform.position.y) / 2) + test);
+
+            transform.position = new Vector3(transform.position.x, cameraPos, transform.position.z);
+            Debug.Log("i andra ");
+        }
+
+
+        else if(players[1].transform.position.y == players[0].transform.position.y)
+        {
+            test = players[1].transform.position.y;
+
+            cameraPos =   test;
+
+            transform.position = new Vector3(transform.position.x, cameraPos, transform.position.z);
+        }
+
+
+
+
+
+
+
+
+
+    }
+
+}
---
+++ Ragdoll Climb/Assets/Test/WaterBoat.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterBoat : MonoBehaviour
+{
+    public GameObject boat;
+    public GameObject water;
+    public Transform endLerp;
+
+    public float speed;
+    float startTime;
+    float journeyLength;
+
+    float timer;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        timer = 30;
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(boat.transform.position, endLerp.position);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        water.transform.position = new Vector3(0, boat.transform.position.y - 0.5f, 0);
+
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        if(timer < 0)
+        {
+            float distCovered = (Time.time - startTime) * speed;
+            float fracJourney = distCovered / journeyLength;
+            transform.position = Vector3.Lerp(boat.transform.position, endLerp.position, fracJourney);
+        }
+
+
+
+
+		
+	}
+}
