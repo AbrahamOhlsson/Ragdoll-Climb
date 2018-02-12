549e9fa2bede2e82cb40b0e1077811a30b75dc1e
UTB\Ohlsab <eyber4@gmail.com>
2018-02-12 13:41:23 +0100

    Block destruction.

---
+++ Ragdoll Climb/Assets/Scripts/destroyBlock.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyBlock : MonoBehaviour {
+
+    public GameObject ship;
+    public float distance;
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (  transform.position.y < (ship.transform.position.y + distance))
+        {
+            Destroy(gameObject);
+        }
+	}
+}
