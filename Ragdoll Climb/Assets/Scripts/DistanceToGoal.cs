f851febbb02ead4736645854af74b39619eae848
Emanuel Holm <emanuel.holm@gmail.com>
2018-02-14 15:52:54 +0100

    Added new prefab - FinishLineIndicator(UI)

---
+++ Ragdoll Climb/Assets/Scripts/DistanceToGoal.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DistanceToGoal : MonoBehaviour {
+
+
+    public Transform goal; // "FinishArea" GameObject
+    public Transform cam;  // "MainCamera" GameObject
+    public Text rangeText;
+
+    float startPos;
+    float distToEnd;
+    float procentToEnd;
+
+
+    void Start ()
+    {
+        // "startPos" is the camera position
+        startPos = cam.position.y;
+        distToEnd = 0;
+    }
+	
+	void Update ()
+    {
+        distToEnd = goal.position.y - cam.position.y;
+
+        // Calculating the dictance by procent
+        Debug.Log("Distance from goal: " + distToEnd + "(start pos: " + startPos);
+        procentToEnd = (cam.position.y - startPos ) / (goal.position.y - startPos ) ;
+        rangeText.text = "Finish line: " + distToEnd;
+
+        // When it reach zero
+        if(distToEnd <= 0)
+        {
+            rangeText.text = "Finish line: ON SIGHT";
+        }
+        else
+        {
+
+        }
+    }
+}
