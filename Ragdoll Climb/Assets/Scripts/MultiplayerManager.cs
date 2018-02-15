078d9936ec4a19908cd69de040cd157ac42fd3d5
Dennis Karlsson <dennis.k9705@gmail.com>
2018-02-15 13:43:09 +0100

    - Fixed bug where the game always said that the red player won. - Applied the combo boos particle system to the player prefab. - MAde the combo boos perticle system into a prefab. - Commented code in MultiplayerManager.

--- Ragdoll Climb/Assets/Scripts/MultiplayerManager.cs
+++ Ragdoll Climb/Assets/Scripts/MultiplayerManager.cs
@@ -5,26 +5,20 @@ using XInputDotNetPure;
 
 public class MultiplayerManager : MonoBehaviour
 {
-    //[SerializeField] GameObject playerPrefab;
-
-    //[SerializeField] Transform[] playerInstantiatePos = new Transform[4];
-
+    // The colors each of the players should have
     [SerializeField] Color[] playerColors;
 
     [SerializeField] List<GameObject> players = new List<GameObject>();
 
     bool[] playerSpawned = new bool[4];
 
+    // States for four XInput game pads
     GamePadState[] states = new GamePadState[4];
-
-
-    void Start ()
-    {
-	}
-
+    
 
     void Update ()
     {
+        // Gets states for all game pads
         for (int i = 0; i < states.Length; i++)
         {
             states[i] = GamePad.GetState((PlayerIndex)i);
@@ -32,18 +26,18 @@ public class MultiplayerManager : MonoBehaviour
 
         for (int i = 0; i < players.Count; i++)
         {
-            if (/*Input.GetButtonDown("XB-start_p" + (i+1))*/states[i].Buttons.Start == ButtonState.Pressed && !playerSpawned[i])
+            // If a player presses 'Start' and hasn't already spawned
+            if (states[i].Buttons.Start == ButtonState.Pressed && !playerSpawned[i])
             {
-                //players.Add(Instantiate(playerPrefab, playerInstantiatePos[i].position, Quaternion.identity));
+                // Activates player and gives it the right player number
                 players[i].SetActive(true);
-
-                //players[players.Count - 1].GetComponent<PlayerController>().playerNr = i+1;
-                //players[i].GetComponent<PlayerController>().playerNr = i + 1;
+                players[i].GetComponent<PlayerController>().playerNr = i + 1;
                 players[i].GetComponent<PlayerController>().SetGamePad(i);
-
-                //Renderer[] renderers = players[players.Count - 1].GetComponentsInChildren<Renderer>();
+                
+                // Gets all renderers in player
                 Renderer[] renderers = players[i].GetComponentsInChildren<Renderer>();
 
+                // Changes color of all renderers
                 for (int j = 0; j < renderers.Length; j++)
                 {
                     renderers[j].material.color = playerColors[i];
