38633dd541029e4268f2cc0aada06e605b219625
LudvigH <ludvig.hallberg@hotmail.com>
2018-02-15 16:08:29 +0100

    adding music and sound manager

---
+++ Ragdoll Climb/Assets/LUDVIG/sound and music/manager/musicAndSoundManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio; 
+using UnityEngine;
+
+
+
+public class musicAndSoundManager : MonoBehaviour {
+
+    public Sound[] sounds;
+    
+	// Use this for initialization
+	void Awake () {
+		foreach (Sound i in sounds)
+        {
+            i.source = gameObject.AddComponent<AudioSource>();
+            i.source.clip = i.clip;
+
+            i.source.volume = i.volume;
+            i.source.pitch = i.pitch;
+
+        }
+
+	}
+
+    void Start()
+    {
+        Play("music");
+    }
+
+
+    public void Play(string name)
+    {
+        Sound s= null;
+
+        foreach (Sound i in sounds)
+        {
+            if(i.name == name)
+            {
+                s = i;
+                s.source.Play();
+            }
+        }
+        if (s == null)
+        {
+            Debug.LogWarning("Sound:" + name + " not found.");
+
+        }
+    }
+}
