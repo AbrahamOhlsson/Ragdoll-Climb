000dc194eb57b7ad8525d4b89b05dedc55f90d04
UTB\holmma <martinpatric94@hotmail.com>
2018-02-21 12:54:16 +0100

    Start of teleport

---
+++ Ragdoll Climb/Assets/Sandbox/Martin/TeleportUp.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportUp : MonoBehaviour {
+
+    [SerializeField] GameObject PlayerCol;
+
+    /////// For the players position and the teleportation position
+    [SerializeField] int playerPos;
+    [SerializeField] int teleportPos;
+
+    ///////When activated, time left until teleportation happens.
+    [SerializeField] float timeToTeleport;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    PlayerCol = other.transform.root.gameObject;
+
+    //    if(PlayerCol.tag == "Player")
+    //    {
+            
+    //    }
+    //    Destroy(gameObjec);
+
+    //    startTeleporting();
+    //}
+
+    //void startTeleporting()
+    //{
+
+
+    //}
+}
