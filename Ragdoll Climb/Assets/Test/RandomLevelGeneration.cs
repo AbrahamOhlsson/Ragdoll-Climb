c8d1a2fe34355e6186a54a96421f0facffc40688
UTB\Ohlsab <eyber4@gmail.com>
2018-02-22 15:48:36 +0100

    Gorilla throw and smoke partical. Imported Unity standard partical assets.

--- Ragdoll Climb/Assets/Test/RandomLevelGeneration.cs
+++ Ragdoll Climb/Assets/Test/RandomLevelGeneration.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class RandomLevelGeneration : MonoBehaviour
 {
     public List<Transform> levelModules = new List<Transform>();
+
+    public Transform goal;
+
     public int moduleAmount = 5;
 
     float modulesDistY = 15;
@@ -19,5 +22,8 @@ public class RandomLevelGeneration : MonoBehaviour
 
             Instantiate(levelModules[randomIndex], new Vector3(0, modulesDistY * i), Quaternion.identity);
         }
+
+        Instantiate(goal, new Vector3(0, modulesDistY * moduleAmount), Quaternion.identity);
+        print(modulesDistY);
     }
 }
