ad6c1316944526ae617ccf825457547ae4336b57
UTB\holmma <martinpatric94@hotmail.com>
2018-02-22 15:01:19 +0100

    Powerups in form of Teleportation and Freeze.

---
+++ Ragdoll Climb/Assets/Sandbox/Martin/PowerUpandDownScripts/TeleportUp.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportUp : MonoBehaviour {
+
+    [SerializeField]
+    GameObject PlayerTP;
+    GameObject particleSys;
+
+    public GameObject teleportPos;
+
+    //public GameObject[] teleportPoints;
+    public List<GameObject> teleportList;
+
+
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        particleSys = gameObject.transform.Find("Particle System").gameObject;
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        foreach(Transform child in transform)
+        {
+            teleportList.Add(child.gameObject);
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(particleSys);
+
+        PlayerTP = other.transform.root.gameObject;
+
+        if(PlayerTP.tag == "Player")
+        {
+            GetTeleportPosition();
+        }
+        Destroy(gameObject);
+    }
+
+    void GetTeleportPosition()
+    {
+        teleportPos = teleportList [Random.Range(0, teleportList.Count-1)];
+        PlayerTP.GetComponent<PlayerPowerups>().StartTeleport(teleportPos.transform.position);
+    }
+}
