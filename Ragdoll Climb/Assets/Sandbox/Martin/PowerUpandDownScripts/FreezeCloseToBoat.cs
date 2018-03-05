d0526f91e1520688ac44d696ff0095f8d7fabefc
UTB\holmma <martinpatric94@hotmail.com>
2018-03-05 14:38:28 +0100

    Freeze timer is changed depending on how close you are to the boat.

---
+++ Ragdoll Climb/Assets/Sandbox/Martin/PowerUpandDownScripts/FreezeCloseToBoat.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeCloseToBoat : MonoBehaviour {
+
+    public GameObject Boat;
+
+    [SerializeField]
+    float tooCloseToBoat;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Boat = FindObjectOfType<teleportBoatCheck>().bout;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (transform.position.y < Boat.transform.position.y + tooCloseToBoat)
+        {
+            GetComponent<FreezePlayerPowerUp>().closeToBoat = true;
+        }
+        if (transform.position.y > Boat.transform.position.y + tooCloseToBoat)
+        {
+            GetComponent<FreezePlayerPowerUp>().closeToBoat = false;
+        }
+
+    }
+}
