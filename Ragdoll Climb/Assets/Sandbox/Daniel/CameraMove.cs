a8e771b9501b03a75aa12031e257def84804579f
Daniel Pineiro Nordlund <daniel.gw8@gmail.com>
2018-03-09 16:07:26 +0100

    Added new textures Added new materials Added new stun effect on player

---
+++ Ragdoll Climb/Assets/Sandbox/Daniel/CameraMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+	
+		public int Speed = 50;
+
+		void Update()
+		{
+			float xAxisValue = Input.GetAxis("Horizontal") * Speed;
+			float zAxisValue = Input.GetAxis("Vertical") * Speed;
+
+			transform.position = new Vector3(transform.position.x + xAxisValue, transform.position.y, transform.position.z + zAxisValue);
+		}
+	}
