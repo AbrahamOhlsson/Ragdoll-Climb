e862963f4aa00e76b7733badd6534b8e12f4bbdc
UTB\Ohlsab <eyber4@gmail.com>
2018-03-13 16:41:26 +0100

    Fix EndModul and camera.

--- Ragdoll Climb/Assets/Scripts/FinishLine.cs
+++ Ragdoll Climb/Assets/Scripts/FinishLine.cs
@@ -31,6 +31,8 @@ public class FinishLine : MonoBehaviour
 
             int playerNr = playerInfo.playerNr;
 
+            print(playerNr);
+
             if (playerNr == 1)
             {
                 p1Wins.enabled = true;
