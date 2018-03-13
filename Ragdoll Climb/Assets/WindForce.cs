aa259c9afc5b7fdfbeeb29d289e568e97b7f7c72
Dennis Karlsson <dennis.k9705@gmail.com>
2018-03-13 14:42:09 +0100

    - Added pushing wind prefab.

---
+++ Ragdoll Climb/Assets/WindForce.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindForce : MonoBehaviour
+{
+    [Range(0f, 100f)]
+    public float force = 10f;
+
+    List<Rigidbody> rbs = new List<Rigidbody>();
+
+
+    private void FixedUpdate()
+    {
+        for (int i = 0; i < rbs.Count; i++)
+        {
+            rbs[i].AddForce(transform.forward * force);
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        rbs.Add(other.GetComponent<Rigidbody>());
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        rbs.Remove(other.GetComponent<Rigidbody>());
+    }
+}
