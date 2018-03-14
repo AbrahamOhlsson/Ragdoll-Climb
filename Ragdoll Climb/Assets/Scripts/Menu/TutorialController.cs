ea32e4cfc52430cc785acd38d116894969ab77fc
Daniel Pineiro Nordlund <daniel.gw8@gmail.com>
2018-03-14 19:43:48 +0100

    Added VFX lightning ball Added VFX lightning bolt
    Moved files in Sandbox/Daniel
    
    Fixed bugs in RandomGeneratedLevel
    
    Fixed bugs in menu

---
+++ Ragdoll Climb/Assets/Scripts/Menu/TutorialController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using XInputDotNetPure;
+
+
+public class TutorialController : MonoBehaviour
+{
+
+	// Controller variables
+	PlayerIndex playerIndex;
+	GamePadState controller;
+	GamePadState prevState;
+
+	private int spriteNumb;
+
+	GameObject playerTutorial;
+
+	// Use this for initialization
+	void Start ()
+	{
+		spriteNumb = 1;
+
+		playerTutorial = GameObject.Find("PlayerTutorial");
+
+		playerTutorial.GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/ragdollTutorial" + spriteNumb);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		prevState = controller;
+		controller = GamePad.GetState(playerIndex);
+
+		if(controller.Buttons.A == ButtonState.Pressed && prevState.Buttons.A == ButtonState.Released)
+		{
+			spriteNumb++;
+
+			if(spriteNumb > 3)
+			{
+				spriteNumb = 1;
+			}
+
+			ChangeImage();
+		}
+
+		if(controller.Buttons.Start == ButtonState.Pressed && prevState.Buttons.Start == ButtonState.Released)
+		{
+			SceneManager.LoadScene("Level_ConstructionSite");
+		}
+	}
+
+	void ChangeImage()
+	{
+		playerTutorial.GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/ragdollTutorial" + spriteNumb);
+    }
+}
