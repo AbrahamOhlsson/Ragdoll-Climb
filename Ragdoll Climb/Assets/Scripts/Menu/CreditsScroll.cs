e59110e3e077d4c15eac42b571504b503b0adc4b
Daniel Pineiro Nordlund <daniel.gw8@gmail.com>
2018-03-27 15:18:44 +0200

    ice menu update with credits.
    Knight testing!

---
+++ Ragdoll Climb/Assets/Scripts/Menu/CreditsScroll.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsScroll : MonoBehaviour {
+
+	[SerializeField] private float scrollSpeed = 10; 
+	private Vector3 originPos;
+
+	void Start()
+	{
+		originPos = transform.localPosition;
+	}
+
+	void Update ()
+	{
+		transform.Translate(new Vector3(0,scrollSpeed) * Time.deltaTime);
+
+		if (transform.localPosition.y >= 400)
+		{
+			transform.localPosition = originPos;
+		}
+	}
+}
---
+++ Ragdoll Climb/Assets/Scripts/Player Scripts/ClothSetActive.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClothSetActive : MonoBehaviour
+{
+	private Cloth cloth;
+	private GameObject player;
+
+	void Start ()
+	{
+		player = gameObject.transform.root.gameObject;
+
+		cloth = GetComponent<Cloth>();
+	}
+
+	void Update()
+	{
+		if (player.activeSelf)
+		{
+			cloth.enabled = true;
+			Destroy(this);
+		}
+	}
+
+}
