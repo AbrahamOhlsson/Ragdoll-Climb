82efb24240fbefe4076e44f116b6020574f261ac
Emanuel Holm <emanuel.holm@gmail.com>
2018-04-06 14:25:20 +0200

    Finished the RespawnPowerUp prefab and RespawnPowerUpNonFloat prefab(For more information, ask Emanuel)

--- Ragdoll Climb/Assets/Scripts/PowerUpandDownScripts/RespawnPowerUp.cs
+++ Ragdoll Climb/Assets/Scripts/PowerUpandDownScripts/RespawnPowerUp.cs
@@ -4,66 +4,91 @@ using UnityEngine;
 
 public class RespawnPowerUp : MonoBehaviour {
 
+
+    /// <summary>
+    /// ***THIS SCRIPT IS FOR FLOATING POWER-UPS LIKE (Anvil Down, Feather Up)
+    /// </summary>
+
+
+    [Tooltip("Put the given powerUp")]
     public GameObject powerUp;
-    private Vector3 powerPos;
-
-    GameObject player;
-
-    float cooldown = 3;
 
 
+    private GameObject player;
+    private float cooldown = 3;
+    private bool touched;
 
-    bool touched;
+
+    //floating 
+    [SerializeField]
+    private float rotateSpeed = 15.0f;
+    [SerializeField]
+    private float floatLength = 0.3f;
+    [SerializeField]
+    private float floatSpeed = 1f;
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
 
     void Start()
     {
-        powerPos = transform.position;
+        posOffset = transform.position;
         Debug.Log(powerUp.transform.position);
-        Instantiate(powerUp, powerPos, transform.rotation);
+        Instantiate(powerUp, posOffset, transform.rotation); // Spawns the powerUp, one time
         touched = false;
-       
     }
 
+
+
+    //  Activates spawn
     private void Update()
     {
+        // Rotate
+        transform.Rotate(new Vector3(0f, Time.deltaTime * rotateSpeed, 0f), Space.World);
+
+        // Float up/down with a Sin()
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * floatSpeed) * floatLength;
+
+        transform.position = tempPos;
+
         if (touched == true)
         {
             cooldown -= Time.deltaTime * 1;
-
-            if (cooldown <= 0)
-            {
-                touched = false;
-                StartCoroutine("spawnObjects");
-                cooldown = 3;
-            }
+                if (cooldown <= 0)
+                {
+                    Debug.Log("Touched = true");
+                    touched = false;
+                    StartCoroutine("spawnObjects");
+                    cooldown = 3;
+                }
         }
-    }
 
+
+
+    }
+    
+    //  Resets Spawn trigger
     private void OnTriggerEnter(Collider other)
     {
-        if(touched == false)
-        { 
+        if (touched == false)
+        {
             player = other.transform.gameObject;
-            if (player.tag == "Player")
-            {
-                Debug.Log("collision!");
-                StartCoroutine("spawnObjects");
-                touched = true;
-            }
+                if (player.tag == "Player")
+                {
+                    Debug.Log("collision!");
+                    touched = true;
+                }
         }
     }
-    //StartCoroutine("spawnObjects"); << use in collision check
 
-
+    //  Spawn generator
     IEnumerator spawnObjects()
     {
-
         yield return new WaitForSeconds(cooldown);
 
         Debug.Log("SPAWNS");
         Debug.Log(powerUp);
-        Instantiate(powerUp, powerPos, transform.rotation);
-        //Instantiate(powerUp, transform.position, transform.rotation);
+        Instantiate(powerUp, posOffset, transform.rotation);
     }
 
 }
