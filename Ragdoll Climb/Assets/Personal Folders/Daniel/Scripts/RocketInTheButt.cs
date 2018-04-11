788a34767f007a04ab9e48b99f8e5b1f9ad5dde9
UTB\holmma <martinpatric94@hotmail.com>
2018-04-11 13:10:51 +0200

    Almost done rocket in the butt

--- Ragdoll Climb/Assets/Personal Folders/Daniel/Scripts/RocketInTheButt.cs
+++ Ragdoll Climb/Assets/Personal Folders/Daniel/Scripts/RocketInTheButt.cs
@@ -4,13 +4,42 @@ using UnityEngine;
 
 public class RocketInTheButt : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public Rigidbody rb;
+
+    private GameObject player;
+    
+    [SerializeField]
+    float rocketTank;
+    [SerializeField]
+    float rocketForce;
+    private float rocketFuel;
+
+
+   public bool flyReady;
+
+    // Update is called once per frame
+    void Update ()
+    {
+		if(flyReady)
+        {
+            rocketFuel += Time.deltaTime * 1;
+            
+            rb.AddForce(transform.forward * rocketForce);
+
+            if (rocketFuel >= rocketTank || flyReady == false)
+            {
+                rocketFuel = 0;
+                flyReady = false;
+                gameObject.SetActive(false);
+            }
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void startTheRocket()
+    {
+        player = transform.Find("Head_M").gameObject;
+        rb = player.GetComponent<Rigidbody>();
+        flyReady = true;
+        print("The Rocket is online!");
+    }
 }
---
+++ Ragdoll Climb/Assets/Personal Folders/Martin/PowerUpTests/StartRocketInTheBut.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartRocketInTheBut : MonoBehaviour {
+
+    private GameObject player;
+
+    //floating 
+    [SerializeField]
+    private float rotateSpeed = 15.0f;
+    [SerializeField]
+    private float floatLength = 0.3f;
+    [SerializeField]
+    private float floatSpeed = 1f;
+    float startPosY;
+    float tempPosY;
+
+
+    void Start()
+    {
+        startPosY = transform.localPosition.y;
+    }
+
+    // Rotate and float up and down
+    void Update()
+    {
+        // Rotate
+        transform.Rotate(new Vector3(0f, Time.deltaTime * rotateSpeed, 0f), Space.World);
+
+        // Float up/down with a Sin()
+        tempPosY = startPosY;
+        tempPosY += Mathf.Sin(Time.fixedTime * Mathf.PI * floatSpeed) * floatLength;
+
+        transform.localPosition = new Vector3(transform.localPosition.x, tempPosY, transform.localPosition.z);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        player = other.transform.root.gameObject;
+
+        if (player.tag == "Player")
+        {
+            RocketInTheButt rocketInTheButt = player.GetComponentInChildren<RocketInTheButt>(true);
+            rocketInTheButt.gameObject.SetActive(true);
+            rocketInTheButt.startTheRocket();
+
+            Destroy(gameObject);
+        }
+        
+    }
+}
