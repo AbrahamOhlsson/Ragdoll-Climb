cec3a762807c9d0067ca5b78e93e7b9af8991f49
Emanuel Holm <emanuel.holm@gmail.com>
2018-04-12 09:20:46 +0200

    Remade the "Back" and "Continue" in Gimp(Have NOT replaced the old ones in Unity yet) + Remaking the Platform Modules in Blender and Unity(Work In Progress)

--- Ragdoll Climb/Assets/Personal Folders/Emanuel/Script Emanuel/PropellerRotator.cs
+++ Ragdoll Climb/Assets/Personal Folders/Emanuel/Script Emanuel/PropellerRotator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PropellerRotator : MonoBehaviour {
 
-    [Range(0, 50)] [Tooltip("The speed of the propeller")]
+    [Range(0, 100)] [Tooltip("The speed of the propeller")]
     public float rotatingSpeed = 10;
 
 	void Update () {
