3618173cf0af0512a55ed8d83573a7c5129b55ae
Daniel Pineiro Nordlund <daniel.gw8@gmail.com>
2018-04-12 11:28:41 +0200

    visual feedback when death occurs
    Rope prefab changes

---
+++ Ragdoll Climb/Assets/Personal Folders/Daniel/Scripts/SkullAndCrossbones.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkullAndCrossbones : MonoBehaviour {
+
+	Transform trans;
+
+	private bool ScaleUp;
+	private bool Go;
+
+	void Start ()
+	{
+		ShowDeath();
+	}
+
+
+	public void ShowDeath()
+	{
+		trans = GetComponent<Transform>();
+
+		trans.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+
+		ScaleUp = true;
+		Go = true;
+	}
+
+
+	void Update ()
+	{
+		if (ScaleUp)
+		{
+			trans.localScale = Vector3.Lerp(trans.localScale, new Vector3(0.1f,0.1f,0.1f), 0.1f);
+		}
+
+		if(Go)
+		{
+			StartCoroutine(Wait());
+
+			Go = false;
+		}
+
+		if (ScaleUp == false)
+		{
+			trans.localScale = Vector3.Lerp(trans.localScale, new Vector3(0.01f,0.01f,0.01f) , 0.1f);
+		}
+	}
+
+	IEnumerator Wait()
+	{
+		yield return new WaitForSeconds(3);
+
+		ScaleUp = false;
+
+		yield return new WaitForSeconds(1);
+
+		Destroy(gameObject);
+	}
+}
