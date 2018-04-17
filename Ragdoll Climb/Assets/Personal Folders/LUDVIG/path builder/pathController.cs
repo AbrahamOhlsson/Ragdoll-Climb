7193bc6f6066f25a418137ebf9f36351afae3887
LudvigH <ludvig.hallberg@hotmail.com>
2018-04-17 14:04:39 +0200

    adding pathBuilder Ez / Norm / Hard

--- Ragdoll Climb/Assets/Personal Folders/LUDVIG/path builder/pathController.cs
+++ Ragdoll Climb/Assets/Personal Folders/LUDVIG/path builder/pathController.cs
@@ -5,14 +5,50 @@ using UnityEngine;
 public class pathController : MonoBehaviour {
 
     public int maxBlock;
+    [Space]
     [HideInInspector]
     public int currentBlock = 1;
 
-    public List <GameObject> bloakList;
+    enum darlingTest { ez, norm, HARD };
+    [Space]
+    [Header("gör inget ATM")]
+    [SerializeField] darlingTest difficulty;
+
+    public List<GameObject> bloakList;
+    [Space]
+    [Header("gör inget ATM")]
+    [SerializeField] List<GameObject> EZbloakList;
+    [SerializeField] List<GameObject> NORMbloakList;
+    [SerializeField] List<GameObject> HARDbloakList;
+   
     [Space]
     public GameObject endBox;
     [Space]
     public GameObject startBox;
+
+
+    // måste hända innan alla andra build script 
+    private void Awake()
+    {
+        if (difficulty == darlingTest.ez)
+        {
+            //bloakList = EZbloakList;
+
+        }
+
+        if (difficulty == darlingTest.norm)
+        {
+           //bloakList = NORMbloakList;
+
+        }
+
+        if (difficulty == darlingTest.HARD)
+        {
+            //bloakList = HARDbloakList;
+
+        }
+
+    }
 
 
     // Use this for initialization
