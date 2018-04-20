9304dbf5683d238c2f27abfc05c75b313731d650
Dennis Karlsson <dennis.k9705@gmail.com>
2018-04-20 14:12:11 +0200

    - Added overview in difficulty group in menu. - Fixed wind stuff, may need additional fixing for each and every placed wind.

--- Ragdoll Climb/Assets/Scripts/Menu/DiffLengthSelection.cs
+++ Ragdoll Climb/Assets/Scripts/Menu/DiffLengthSelection.cs
@@ -10,28 +10,52 @@ public class DiffLengthSelection : MonoBehaviour
     [SerializeField] Slider lengthSlider;
     [SerializeField] Text diffText;
     [SerializeField] Text lengthText;
+    [SerializeField] GameObject[] blocks;
+    [SerializeField] GameObject[] characterImages;
+    //[SerializeField] Sprite[] characterSprites;
     [SerializeField] string[] diffNames;
     [SerializeField] string[] lengthNames;
 
+    PlayerInfoSingleton singleton;
 
-    private void Start()
+
+    private void Awake()
+    {
+        singleton = PlayerInfoSingleton.instance;
+    }
+
+
+    private void OnEnable()
     {
         UpdateDiffSlider();
         UpdateLengthSlider();
+
+        for (int i = 0; i < characterImages.Length; i++)
+        {
+            characterImages[i].SetActive(i < singleton.playerAmount);
+            //characterImages[i].GetComponent<Image>().sprite = characterSprites[singleton.characterIndex[i]];
+            characterImages[i].GetComponent<Image>().color = singleton.colors[i];
+        }
     }
 
 
     public void UpdateDiffSlider()
     {
         diffText.text = diffNames[Mathf.RoundToInt(diffSlider.value)];
-        PlayerInfoSingleton.instance.levelDifficulty = (PlayerInfoSingleton.Difficulties)Mathf.RoundToInt(diffSlider.value);
+        singleton.levelDifficulty = (PlayerInfoSingleton.Difficulties)Mathf.RoundToInt(diffSlider.value);
     }
 
 
     public void UpdateLengthSlider()
     {
         lengthText.text = lengthNames[Mathf.RoundToInt(lengthSlider.value - 1)];
-        PlayerInfoSingleton.instance.levelLength = (PlayerInfoSingleton.Lengths)Mathf.RoundToInt(lengthSlider.value);
+
+        for (int i = 0; i < blocks.Length; i++)
+        {
+            blocks[i].gameObject.SetActive(i <= lengthSlider.value - 1);
+        }
+
+        singleton.levelLength = (PlayerInfoSingleton.Lengths)Mathf.RoundToInt(lengthSlider.value);
     }
 
 
