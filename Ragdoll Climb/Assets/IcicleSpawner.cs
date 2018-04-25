90ff457b17733d365b865921595d99bd3c443d05
Dennis Karlsson <dennis.k9705@gmail.com>
2018-04-25 18:22:52 +0200

    - Added icicle spawner that spawns icicles that won't respawn. - Maybe fixed bug that caused icicles to grow infinitely. - Icicles will now not get destroyed by the first object it touches. Icicles should be place a bit inside roofs. - Icicles is now destroyed when below the bottom object.

---
+++ Ragdoll Climb/Assets/IcicleSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IcicleSpawner : MonoBehaviour
+{
+    [SerializeField] float spawnDelay = 3f;
+    [SerializeField] GameObject icicle;
+
+    float timer = 0;
+    
+    Transform bottomObj;
+
+    Vector3 scale;
+
+
+	void Awake ()
+    {
+        scale = transform.GetChild(0).localScale;
+
+        transform.GetChild(0).GetComponent<Icicle>().instantiated = true;
+
+        bottomObj = GameObject.FindGameObjectWithTag("BottomObj").transform;
+    }
+
+
+    void Update ()
+    {
+        if (timer >= spawnDelay)
+        {
+            GameObject inst = Instantiate(icicle, transform);
+            inst.transform.localScale = scale;
+            inst.GetComponent<Icicle>().instantiated = true;
+
+            timer = 0;
+        }
+        else
+            timer += Time.deltaTime;
+
+        if (transform.position.y <= bottomObj.position.y)
+            Destroy(gameObject);
+    }
+}
