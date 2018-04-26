a80e08544c2edc602362e2a1751615165b74ad12
Dennis Karlsson <dennis.k9705@gmail.com>
2018-04-26 10:14:38 +0200

    - Added new type of icicle that never drops and starts to grow when a player enters a trigger.

---
+++ Ragdoll Climb/Assets/Scripts/Icicles/IcicleGrowthTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IcicleGrowthTrigger : MonoBehaviour
+{
+    Icicle_NonDrop icicle;
+
+
+    private void Awake()
+    {
+        icicle = GetComponentInParent<Icicle_NonDrop>();
+        transform.SetParent(transform.parent.parent);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            icicle.grow = true;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Ragdoll Climb/Assets/Scripts/Icicles/Icicle_NonDrop.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Icicle_NonDrop : MonoBehaviour
+{
+    public float growthSpeed = 0.1f;
+    
+    internal bool grow = false;
+
+    float targetScale;
+    float minScale = 0;
+    float scale;
+    
+
+    void Start()
+    {
+        targetScale = transform.localScale.x;
+        transform.localScale = new Vector3(minScale, minScale, minScale);
+        scale = transform.localScale.x;
+    }
+
+    void Update()
+    {
+        if (grow)
+        {
+            scale += growthSpeed * Time.deltaTime;
+            transform.localScale = new Vector3(scale, scale, scale);
+
+            if (scale >= targetScale)
+                grow = false;
+        }
+    }
+}
