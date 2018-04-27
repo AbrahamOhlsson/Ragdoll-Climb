55516a6bcebcce433a5ca6d53f4925be8c18cd0f
LudvigH <ludvig.hallberg@hotmail.com>
2018-04-27 15:43:07 +0200

    adding save .... in singleplayer lvl

---
+++ Ragdoll Climb/Assets/Scripts/Singleplayer/starDestroy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class starDestroy : MonoBehaviour {
+
+    GameObject Player;
+    bool canGiveStar;
+
+    private void Start()
+    {
+        canGiveStar = true;    
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Player = other.transform.root.gameObject;
+
+        if (Player.tag == "Player"&& canGiveStar)
+        {
+            canGiveStar = false;
+            Player.transform.GetComponent<singleplayerInfo>().stars++;
+            Destroy(gameObject);
+            
+        }
+    }
+}
