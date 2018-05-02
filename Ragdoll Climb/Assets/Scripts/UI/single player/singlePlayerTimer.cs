eca87ef342fa0cab937fd00e5102006364d122dd
LudvigH <ludvig.hallberg@hotmail.com>
2018-05-02 16:03:45 +0200

    update SP_ice and some sound/music

---
+++ Ragdoll Climb/Assets/Scripts/UI/single player/singlePlayerTimer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class singlePlayerTimer : MonoBehaviour {
+
+    Text timeText;
+
+	// Use this for initialization
+	void Start () {
+       // timeText = FindObjectOfType<singleplayerInfo>().playtime.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
