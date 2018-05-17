83f804c95296dd9430c6c771510713d4705e8dd9
Daniel Pineiro Nordlund <daniel.gw8@gmail.com>
2018-05-17 13:40:23 +0200

    singleplayer level

---
+++ Ragdoll Climb/Assets/Scripts/Nail/ReleaseRope.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReleaseRope : MonoBehaviour
+{
+	[SerializeField]
+	private Rigidbody ropeRb;
+	private Rigidbody nailRb;
+
+	[SerializeField]
+	BreakingSurface breakingSurface;
+
+	private float nailForce = -20;
+	private float nailRotation = -30;
+
+
+	void Start ()
+	{
+		nailRb = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	void Update ()
+	{
+		if(breakingSurface.broken)
+		{
+			RopeSetFree();
+			nailRb.AddForce(transform.up * nailForce);
+			nailRb.AddTorque(transform.right * nailRotation);
+		}
+	}
+
+	void RopeSetFree()
+	{
+		ropeRb.isKinematic = false;
+		nailRb.isKinematic = false;
+		
+	}
+
+}
