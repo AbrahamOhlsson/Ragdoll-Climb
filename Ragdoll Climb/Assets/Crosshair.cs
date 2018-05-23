95ce1884d8733ac6a0e65c8bb713746e61a786ab
Dennis Karlsson <dennis.k9705@gmail.com>
2018-05-23 14:20:47 +0200

    - Added crosshair over players targeted by penguins. - Added ability to get the spine object of players from PlayerInfo.

--- Ragdoll Climb/Assets/Crosshair.cs
+++ Ragdoll Climb/Assets/Crosshair.cs
@@ -2,36 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PenguinScout : MonoBehaviour
+public class Crosshair : MonoBehaviour
 {
     float minAlpha = 0.4f;
     float maxAlpha = 0.8f;
     float targetAlpha;
     float currAlpha;
-    float speed = 5f;
+    float speed = 6f;
 
-    Penguin penguin;
     Material mat;
 
 
-    void Start ()
+    void Awake ()
     {
-        penguin = transform.parent.GetComponent<Penguin>();
         mat = GetComponent<Renderer>().material;
 
         targetAlpha = minAlpha;
         currAlpha = maxAlpha;
-        mat.color = new Color(1, 1, 0, 0);
     }
 
 
-    public void SetColor(Vector3 rgb)
-    {
-        mat.color = new Color(rgb.x, rgb.y, rgb.z, currAlpha);
-    }
-
-
-    private void Update()
+    void Update ()
     {
         if (Mathf.Abs(mat.color.a - targetAlpha) <= 0.01f)
         {
@@ -44,16 +35,7 @@ public class PenguinScout : MonoBehaviour
         currAlpha = Mathf.Lerp(currAlpha, targetAlpha, speed * Time.deltaTime);
 
         mat.color = new Color(mat.color.r, mat.color.g, mat.color.b, currAlpha);
-    }
 
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (penguin.state == Penguin.PenguinStates.Scout && other.tag == "Player")
-        {
-            penguin.PrepareLaunch(other.transform.root.GetComponent<PlayerInfo>().spine.transform);
-
-            mat.color = new Color(1, 0, 0, currAlpha);
-        }
+        transform.eulerAngles = Vector3.zero;
     }
 }
