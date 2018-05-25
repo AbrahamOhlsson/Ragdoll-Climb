cf862bb41e39bcab5661d6a1f68e7f3cae6a5217
Dennis Karlsson <dennis.k9705@gmail.com>
2018-05-25 13:35:51 +0200

    - Added level progression. Completing a level unlocks the next. - Mannequin is no longer available until 99 keys are collected.

--- Ragdoll Climb/Assets/Scripts/SP_LevelStats.cs
+++ Ragdoll Climb/Assets/Scripts/SP_LevelStats.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class SP_LevelStats 
 {
+    public bool completed = false;
     public int starAmount = 0;
     public float bestTime_flt = Mathf.Infinity;
     public string bestTime_str = "--:--:--";
