ecb513a2fd29e494daf1da6bf771c4b45831d11b
UTB\holmma <martinpatric94@hotmail.com>
2018-05-27 22:08:18 +0200

    The last hard and medium singleplayer level

---
+++ Ragdoll Climb/Assets/Scripts/Surfaces/BaybladeRotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaybladeRotator : MonoBehaviour {
+
+    [Range(0, 100)] [Tooltip("The speed of the propeller")]
+    public float spinSpeed = 10;
+
+	void Update () {
+        transform.Rotate(new Vector3(0, Time.deltaTime * spinSpeed, 0));
+	}
+}
