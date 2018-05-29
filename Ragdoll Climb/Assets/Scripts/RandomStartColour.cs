7a2970641e547cc25abdf67313c8a9d23d704efe
Daniel Pineiro Nordlund <daniel.gw8@gmail.com>
2018-05-29 16:56:47 +0200

    New menu!
    Trees and bushes pack added
    
    Castle theme pack added
    
    UI color fixes
    
    Knight random colour script now also changes the colour of the cloth (in menu)
    
    Occlusion baked in main menu

--- Ragdoll Climb/Assets/Scripts/RandomStartColour.cs
+++ Ragdoll Climb/Assets/Scripts/RandomStartColour.cs
@@ -6,18 +6,22 @@ public class RandomStartColour : MonoBehaviour {
 
 
 	private Renderer rend;
+	[SerializeField]
+	private Renderer CRend;
 
 	public Color[] colors;
 
 	void Start()
 	{
 		rend = GetComponent<Renderer>();
+
 		if (colors == null || colors.Length < 2)
 		{
 			Debug.Log("Need to setup colors array in inspector");
 		}
 
 		rend.material.color = colors[Random.Range(0, colors.Length)];
+		CRend.material.color = rend.material.color;
 
 	}
 
